50bf97a5776ff4658122e34703f7059180a7bee7
gopinath0332 <gopinath0332@users.noreply.github.com>
2018-02-20 00:53:00 +0530

    Initial commit

---
+++ src/main/app/config/webpack.common.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+
+module.exports = {
+   entry: {
+      app: "./src/index.js"
+   },
+   output: {
+      filename: "[name].build.js",
+      path: path.resolve(__dirname, "../../webapp/resources/dist")
+   },
+   plugins: [
+      new CleanWebpackPlugin(['dist']),
+   ],
+   module: {
+      rules: [{
+         test: /\.css$/,
+         use: [
+            'style-loader', 'css-loader'
+         ]
+      }]
+   }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<script src="./resources/dist/app.build.js"></script>
+</body>
+</html>
