382d024608efa90f897d8aefd6c25162950bcf35
Gopinath Rajendra <gopinath0332@outlook.com>
2018-02-20 01:30:35 +0530

    Adding react js implementation

--- src/main/app/config/webpack.common.js
+++ src/main/app/config/webpack.common.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 
 module.exports = {
@@ -21,6 +22,21 @@ module.exports = {
          use: [
             'style-loader', 'css-loader'
          ]
-      }]
+      },{
+          test: /\.js?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel-loader',
+          query: {
+              presets: ['react', 'es2015', 'stage-0',"es2017"],
+              plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy',"transform-object-rest-spread"],
+          }
+      },  {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+          {
+              test: /\.less$/,
+              loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+          }]
    }
 }
--- src/main/app/src/index.js
+++ src/main/app/src/index.js
@@ -1,1 +1,11 @@
-console.log("app index js  entry file");
+import React,{Component} from "react";
+import ReactDOM from "react-dom";
+class  App extends  Component{
+    render(){
+        return(
+            <div> App component - 123</div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>,document.getElementById("content"));
